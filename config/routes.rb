1f62f64d58371b3f16915ec487a7cf5037363d29
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-04 15:04:43 +0000

    added initial controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BookItPractice::Application.routes.draw do
+  resources :resources do
+	  resources :bookings
+	end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
